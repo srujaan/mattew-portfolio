d4c69afc2c21c82528d6d74bda208b10b4c628dd
Matthew Secrist <matthew.secrist@outlook.com>
2019-02-12 21:59:29 -0600

    More stuff

---
+++ src/components/PostListing.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import moment from 'moment'
+import { Link } from 'gatsby'
+
+const PostTitle = styled.h1`
+  padding: 0;
+  margin: 0;
+  font-size: 2.5em;
+`
+
+const PostListing = ({
+  post: {
+    node: {
+      uid,
+      data: { date, title, description }
+    }
+  }
+}) => {
+  return (
+    <React.Fragment key={`${uid}`}>
+      <Link to={`/blog/${uid}`}>
+        <PostTitle key={`${uid}-title`}>{title.text}</PostTitle>
+      </Link>
+      <sub>Posted {moment(date).format('LL')}</sub>
+      <p key={`${uid}-subtitle`}>{description}</p>
+    </React.Fragment>
+  )
+}
+
+export default PostListing
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../layout/layout'
-import styled from 'styled-components'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
+import PostListing from '../components/PostListing'
 
 const BlogPage = ({
   data: {
@@ -10,23 +10,9 @@ const BlogPage = ({
 }) => {
   return (
     <Layout>
-      {posts.map(post => {
-        let {
-          node: {
-            uid,
-            data: { date, title, description }
-          }
-        } = post
-
-        return (
-          <React.Fragment key={`${uid}`}>
-            <Link to={`/blog/${uid}`}>
-              <h2 key={`${uid}-title`}>{title.text}</h2>
-            </Link>
-            <p key={`${uid}-subtitle`}>{description}</p>
-          </React.Fragment>
-        )
-      })}
+      {posts.map(post => (
+        <PostListing post={post} />
+      ))}
     </Layout>
   )
 }
@@ -35,7 +21,7 @@ export default BlogPage
 
 export const pageQuery = graphql`
   {
-    allPrismicPost {
+    allPrismicPost(sort: { fields: [data___date], order: DESC }) {
       edges {
         node {
           uid
