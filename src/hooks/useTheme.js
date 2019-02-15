bda44883ef1c7c866d2815efb601be01e0ab44fd
Matthew Secrist <matthew.secrist@outlook.com>
2019-02-15 15:34:17 -0600

    Inspired by use-dark-mode fix

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -5,31 +5,10 @@ const isBrowser = typeof window !== 'undefined'
 
 const initialState = () => {
   if (isBrowser) {
-    const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-
-    const userPrefersDarkMode = JSON.parse(
-      window.localStorage.getItem('darkMode')
-    )
-
-    if (userPrefersDarkMode === null) {
-      window.localStorage.setItem('darkMode', darkMode)
-    }
-
-    return darkMode || userPrefersDarkMode ? dark : light
+    return window.__theme === 'dark' ? dark : light
   }
 
-  return {
-    colors: {
-      name: 'server',
-      background: 'transparent',
-      text: 'black',
-      primary: 'black',
-      navBg: 'white',
-      navText: 'black',
-      semiDark: 'black',
-      codeBg: 'black'
-    }
-  }
+  return light
 }
 
 const useTheme = () => {
@@ -39,9 +18,10 @@ const useTheme = () => {
 
   const toggleTheme = () => {
     let darkMode = theme === dark
+    let inverseTheme = darkMode ? 'light' : 'dark'
 
     if (isBrowser) {
-      window.localStorage.setItem('darkMode', !darkMode)
+      window.__setPreferredTheme(inverseTheme)
     }
     switchTheme(darkMode ? light : dark)
   }
