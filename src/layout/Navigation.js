fac3592e71e5c54aa86023bf963cbcc05002a7f8
Matthew Secrist <matthew.secrist@outlook.com>
2019-02-15 18:44:40 -0600

    More nav fixes

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -3,46 +3,50 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { md } from 'styled-bootstrap-responsive-breakpoints'
 import { MenuAltRight } from 'styled-icons/boxicons-regular/MenuAltRight'
-import useWindowWidth from '../hooks/useWindowWidth'
-
-const NavBar = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  flex-direction: column;
-  top: 0;
-  height: 100vh;
-  position: sticky;
-`
 
 const MobileNavMenu = styled(MenuAltRight)`
   position: fixed;
   top: 0;
   right: 0;
-  color: ${({ visible }) => (visible ? 'var(--text)' : 'var(--primary)')};
+  color: ${props => (props.toggled ? 'var(--text)' : 'var(--primary)')};
   z-index: 100;
+  @media screen and (min-width: ${md}px) {
+    visibility: hidden;
+  }
 `
 
-const MobileNav = styled.div`
-  background: var(--navBg);
-  position: fixed;
-  z-index: 90;
-  display: flex;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: 200px;
-  border-left: 1px solid var(--primary);
-  justify-content: center;
-  align-items: flex-end;
-  flex-direction: column;
+const NavMenu = styled.div`
+  @media screen and (min-width: ${md}px) {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    flex-direction: column;
+    top: 0;
+    height: 100vh;
+    position: sticky;
+  }
 
-  transition: transform 0.3s ease-in-out;
-  transform: translateX(${props => (props.visible ? '0%' : '100%')});
+  @media screen and (max-width: ${md}px) {
+    background: var(--navBg);
+    position: fixed;
+    z-index: 90;
+    display: flex;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 200px;
+    border-left: 1px solid var(--primary);
+    justify-content: center;
+    align-items: flex-end;
+    flex-direction: column;
 
-  a {
-    opacity: ${props => props.visible};
-    transition: opacity 0.3s 0.3s ease-in-out;
+    transition: transform 0.3s ease-in-out;
+    transform: translateX(${props => (props.visible ? '0%' : '100%')});
+
+    a {
+      opacity: ${props => props.visible};
+      transition: opacity 0.3s 0.3s ease-in-out;
+    }
   }
 `
 
@@ -59,9 +63,6 @@ const NavLinksContainer = styled.div`
   `
 
 const Navigation = () => {
-  const width = useWindowWidth()
-  const isMobile = width <= md
-
   const [navExpanded, toggleNavExpand] = useState(false)
 
   const NavLinks = () => (
@@ -84,24 +85,20 @@ const Navigation = () => {
     </NavLinksContainer>
   )
 
-  const mobileNav = () => {
+  const Nav = () => {
     return (
       <React.Fragment>
         <MobileNavMenu
           size={48}
-          visible={!navExpanded}
+          toggled={!navExpanded}
           onClick={() => toggleNavExpand(!navExpanded)}
         />
-        <MobileNav visible={navExpanded}>{NavLinks()}</MobileNav>
+        <NavMenu visible={navExpanded}>{NavLinks()}</NavMenu>
       </React.Fragment>
     )
   }
 
-  const Nav = () => {
-    return <NavBar>{NavLinks()}</NavBar>
-  }
-
-  return isMobile ? mobileNav() : Nav()
+  return Nav()
 }
 
 export default Navigation
