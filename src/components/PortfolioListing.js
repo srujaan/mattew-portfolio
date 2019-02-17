a9be4734891066617c3c4be475510940c41d6ff1
Matthew Secrist <matthew.secrist@outlook.com>
2019-02-17 10:41:13 -0600

    Remove hover hook

--- src/components/PortfolioListing.js
+++ src/components/PortfolioListing.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 import styled from 'styled-components'
 
-import useHover from '../hooks/useHover'
-
 const Card = styled.div`
   background: url(${props => props.img});
   height: 300px;
@@ -12,57 +10,63 @@ const Card = styled.div`
   overflow: hidden;
   filter: grayscale(100%);
 
+  & > .card-overlay {
+    background: rgba(0, 0, 0, 0.5);
+    position: relative;
+    height: 100%;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    opacity: 0;
+    -webkit-transition: all 0.4s ease-in-out 0s;
+    -moz-transition: all 0.4s ease-in-out 0s;
+    transition: all 0.4s ease-in-out 0s;
+
+    & > .card-details {
+      position: absolute;
+      text-align: center;
+      top: 0%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      -moz-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      -webkit-transition: all 0.3s ease-in-out 0s;
+      -moz-transition: all 0.3s ease-in-out 0s;
+      transition: all 0.3s ease-in-out 0s;
+      opacity: 0;
+
+      p {
+        color: white;
+      }
+    }
+  }
+
   &:hover {
     filter: grayscale(0);
     box-shadow: 0 0 20px #000000;
-  }
-`
-
-const CardDetails = styled.div`
-  background: rgba(0, 0, 0, 0.5);
-  position: relative;
-  height: 100%;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  opacity: ${props => (props.isHovered ? 80 : 0)};
-  -webkit-transition: all 0.4s ease-in-out 0s;
-  -moz-transition: all 0.4s ease-in-out 0s;
-  transition: all 0.4s ease-in-out 0s;
-`
-
-const CardTitle = styled.div`
-  position: absolute;
-  text-align: center;
-  top: ${props => (props.isHovered ? '50%' : '0%')};
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  -webkit-transition: all 0.3s ease-in-out 0s;
-  -moz-transition: all 0.3s ease-in-out 0s;
-  transition: all 0.3s ease-in-out 0s;
-  opacity: ${props => (props.isHovered ? 1 : 0)};
-
-  p {
-    color: white;
+    & > .card-overlay {
+      opacity: 1;
+      & > .card-details {
+        top: 50%;
+        opacity: 1;
+      }
+    }
   }
 `
 
 const PortfolioListing = ({ img }) => {
-  const [hoverRef, isHovered] = useHover()
   return (
-    <Card ref={hoverRef} img={img}>
-      <CardDetails isHovered={isHovered}>
-        <CardTitle isHovered={isHovered}>
+    <Card img={img}>
+      <div className='card-overlay'>
+        <div className='card-details'>
           <h1>Project Title</h1>
           <p>Lorem ipsum dolar set.</p>
           <a>View Project</a>
-        </CardTitle>
-      </CardDetails>
+        </div>
+      </div>
     </Card>
   )
 }
 
-export default PortfolioListing
+export default React.memo(PortfolioListing)
