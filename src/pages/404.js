f6530d51d374d4f9241e94b23a08ba1644a384ec
Matthew Secrist <matthew.secrist@outlook.com>
2019-02-19 09:10:21 -0600

    fix broken 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'gatsby'
 import Layout from '../layout/layout'
+
 import SEO from '../components/SEO'
 import SuperText from '../components/SuperText'
 
-export default () => {
+const Center = styled.div`
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const FourOhFour = () => {
   return (
     <Layout>
       <SEO title='FOUR OH FOUR!' />
-      <Container>
-        <SuperText size='3em'>Four Oh Four</SuperText>
+      <Center>
+        <SuperText size='3em'>FourOhFour</SuperText>
         <p>This doesn't seem to be a page.</p>
         <Link to='/'>Go Home</Link>
-      </Container>
+      </Center>
     </Layout>
   )
 }
+
+export default FourOhFour
