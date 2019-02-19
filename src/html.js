013e1bbc8f9fabc2951c3454cdd55d5b4577f4d1
Matthew Secrist <matthew.secrist@outlook.com>
2019-02-18 21:00:16 -0600

    fix preconnect

--- src/html.js
+++ src/html.js
@@ -12,7 +12,11 @@ export default function HTML (props) {
           name='viewport'
           content='width=device-width, initial-scale=1, shrink-to-fit=no'
         />
-        <link rel='preconnect' href='https://fonts.gstatic.com/' crossOrigin />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com/'
+          crossOrigin='true'
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
